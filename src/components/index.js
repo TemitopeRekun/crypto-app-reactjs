a47d640848adf9678a3c82637224b21ccab78a42
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-10-03 23:35:17 +0100

    added the components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 import Navbar from "./Navbar";
+import Homepage from "./Homepage";
+import CryptoDetails from "./CryptoDetails";
+import Cryptocurrencies from "./Cryptocurrencies";
+import News from "./News";
+import Exchanges from "./Exchanges";
 
-export { Navbar };
+export { Navbar, Homepage, CryptoDetails, Cryptocurrencies, News, Exchanges };
