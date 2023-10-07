cce3f66a56b02308a08469c723109a967df58c55
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-10-07 23:20:16 +0100

    preparing headers

--- src/Services/CryptoApi.js
+++ src/Services/CryptoApi.js
@@ -7,12 +7,14 @@ const cryptoApiHeaders = {
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
 
-export cryptoApi = createApi({
-    reducerPath: "cryptoApi",
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => ({
-        getCryptos: builder.query({
-            query: () => "/"
-        })
-    })
-})
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+	reducerPath: "cryptoApi",
+	baseQuery: fetchBaseQuery({ baseUrl }),
+	endpoints: (builder) => ({
+		getCryptos: builder.query({
+			query: () => createRequest("/exchanges"),
+		}),
+	}),
+});
