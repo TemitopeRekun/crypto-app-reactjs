e7e36cec8e8b92231e761c8a6b602793e99188c5
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-10-07 22:58:49 +0100

    creating the end points

---
+++ src/Services/CryptoApi.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+	"X-RapidAPI-Key": "832a1d22b6mshff8bc74eb0155d9p17c636jsnb3765635460e",
+	"X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+export cryptoApi = createApi({
+    reducerPath: "cryptoApi",
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => "/"
+        })
+    })
+})
